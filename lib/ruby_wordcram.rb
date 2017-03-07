91fcf5433e78163044c5631a0e5f99311fd20bef
monkstone <mamba2928@yahoo.co.uk>
2017-03-07 14:46:42 +0000

    expand support for Placers

--- lib/ruby_wordcram.rb
+++ lib/ruby_wordcram.rb
@@ -3,7 +3,10 @@ if RUBY_PLATFORM == 'java'
   require 'WordCram.jar'
   require 'jsoup-1.7.2.jar'
   require 'cue.language.jar'
-  %w(Colorers Word WordAngler WordColorer WordCram WordFonter WordPlacer).each do |klass|
+  wc = %w(WordAngler WordColorer WordCram WordFonter WordPlacer WordSkipReason)
+  sh = %w(Colorers ImageShaper Placers Word ShapeBasedPlacer)
+  WC = wc.concat(sh).freeze
+  WC.each do |klass|
     java_import "wordcram.#{klass}"
   end
 end
