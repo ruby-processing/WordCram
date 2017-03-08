e2f1ffd6f1bfc4218377853602f0a5e9b29e999e
monkstone <mamba2928@yahoo.co.uk>
2017-03-08 10:46:15 +0000

    Observer

--- lib/ruby_wordcram.rb
+++ lib/ruby_wordcram.rb
@@ -4,7 +4,7 @@ if RUBY_PLATFORM == 'java'
   require 'jsoup-1.7.2.jar'
   require 'cue.language.jar'
   wc = %w(WordAngler WordColorer WordCram WordFonter WordPlacer WordSkipReason)
-  sh = %w(Colorers ImageShaper Placers Word ShapeBasedPlacer)
+  sh = %w(Colorers ImageShaper Observer Placers Word ShapeBasedPlacer)
   WC = wc.concat(sh).freeze
   WC.each do |klass|
     java_import "wordcram.#{klass}"
